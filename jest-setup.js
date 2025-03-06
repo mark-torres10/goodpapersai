7bac909b0c467fcaff303f2a3f237298712da088
mark-torres10 <markptorres1@gmail.com>
2025-03-05 21:08:28 -0500

    Fix test configuration: add fetch polyfill and remove duplicate mocks

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,9 @@
 // Jest setup file
 import '@testing-library/jest-dom';
 
+// Add fetch polyfill for Node.js environment
+global.fetch = require('node-fetch');
+
 // Add any global test setup needed here
 
 // Silence console errors during tests
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/goodpapers/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/goodpapers/__mocks__/fileMock.js',
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
@@ -12,4 +12,17 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   testPathIgnorePatterns: ['/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // Set different environments for different test directories
+  projects: [
+    {
+      displayName: 'server',
+      testMatch: ['<rootDir>/tests/server/**/*.test.js'],
+      testEnvironment: 'node',
+    },
+    {
+      displayName: 'client',
+      testMatch: ['<rootDir>/goodpapers/src/**/*.test.(js|jsx|ts|tsx)'],
+      testEnvironment: 'jsdom',
+    }
+  ]
 }; 
